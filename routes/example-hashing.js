4aa1e4e3c1902d6553b66e4f49d4089e6c8dd251
rokity <riccardo.amadio1@gmail.com>
2018-07-01 17:38:06 +0200

    es6 linting and add options CORS

--- routes/example-hashing.js
+++ routes/example-hashing.js
@@ -6,13 +6,16 @@ module.exports = [
         path: '/api/hashing/',
         handler: (request, response) => {
             var toHash = request.payload.tohash;
-             return response.response(JSON.stringify({hashed:hashIt(toHash)}))
-      }
+            return response.response(JSON.stringify({ hashed: hashIt(toHash) }))
+        },
+        options:{
+            cors:true
+        }
     }
 ]
 
-function hashIt(toHash){
-var md = forge.md.sha512.create();
-md.update(toHash);
-return md.digest().toHex();
+let hashIt = (toHash) => {
+    var md = forge.md.sha512.create();
+    md.update(toHash);
+    return md.digest().toHex();
 }
